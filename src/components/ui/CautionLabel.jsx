f19355e9194dfb48b724cbb28bf4f0115cfe56c3
Thom Sterenborg <thomsterenborg@gmail.com>
2023-02-28 18:09:50 +0100

    Added Tooltip to Caution labels

--- src/components/ui/CautionLabel.jsx
+++ src/components/ui/CautionLabel.jsx
@@ -1,19 +1,23 @@
 import { WarningTwoIcon } from "@chakra-ui/icons";
-import { Tag, TagLeftIcon } from "@chakra-ui/react";
+import { Tag, TagLeftIcon, Tooltip } from "@chakra-ui/react";
 
 //Generates a caution-tag for the parsed label
 
 export const CautionLabel = ({ label }) => {
   return (
-    <Tag
-      size="sm"
-      bg="red.100"
-      textTransform="uppercase"
-      color="red.600"
-      fontSize={10}
+    <Tooltip
+      label={`This recipe contains \"${label}\" and may cause an allergic reaction`}
     >
-      <TagLeftIcon as={WarningTwoIcon}></TagLeftIcon>
-      {label}
-    </Tag>
+      <Tag
+        size="sm"
+        bg="red.100"
+        textTransform="uppercase"
+        color="red.600"
+        fontSize={10}
+      >
+        <TagLeftIcon as={WarningTwoIcon}></TagLeftIcon>
+        {label}
+      </Tag>
+    </Tooltip>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RecipesPage } from './pages/RecipesPage';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecipesPage } from "./pages/RecipesPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<ChakraProvider>
-			<RecipesPage />
-		</ChakraProvider>
-	</React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <RecipesPage />
+    </ChakraProvider>
+  </React.StrictMode>
 );
